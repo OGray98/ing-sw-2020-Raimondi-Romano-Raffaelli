19824c45059988862def05628721b643c71f4114
daxus4 <davide.raffaellii@gmail.com>
2020-04-05 15:12:11 +0200

    Commit because i want to update project

--- src/main/java/it/polimi/ingsw/model/player/ArtemisDecorator.java
+++ src/main/java/it/polimi/ingsw/model/player/ArtemisDecorator.java
@@ -1,8 +1,24 @@
 package it.polimi.ingsw.model.player;
 
 
+import it.polimi.ingsw.model.board.Position;
 
 public class ArtemisDecorator extends PlayerMoveDecorator {
 
     public ArtemisDecorator(PlayerInterface player){ super(player);}
+
+    /*
+    @Override
+    public boolean canUsePower(Position pos) {
+
+        return player.canMove(player.getBoard().getAdjacentCells(), pos, player.getBoard().getCell().getLevel());
+    }
+    */
+
+    /*
+    @Override
+    public void usePower(Position pos) {
+        player.moveWorker(pos, workerIndex);
+    }
+    */
 }
