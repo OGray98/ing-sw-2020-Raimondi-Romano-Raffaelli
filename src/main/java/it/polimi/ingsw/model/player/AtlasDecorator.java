4db51eff8eb0e1d536556af2b3736ad612f0423f
romano-francesco <francesco6.romano@mail.polimi.it>
2020-04-05 15:31:20 +0200

    Light modifies on AtlasDecorator

--- src/main/java/it/polimi/ingsw/model/player/AtlasDecorator.java
+++ src/main/java/it/polimi/ingsw/model/player/AtlasDecorator.java
@@ -1,24 +1,15 @@
 package it.polimi.ingsw.model.player;
 
-
-
-import it.polimi.ingsw.exceptions.InvalidPositionException;
-
-import it.polimi.ingsw.model.board.Cell;
 import it.polimi.ingsw.model.board.Position;
-
-import java.util.List;
 
 // Decorator that decorate the special power of Atlas
 public class AtlasDecorator extends PlayerBuildDecorator {
 
-
-
     protected AtlasDecorator(PlayerInterface player){ super(player);}
 
     @Override
-    public boolean canBuild(List<Cell> adjacentCells, Position buildingPosition) throws InvalidPositionException, NullPointerException {
-        if(super.canBuild(adjacentCells,buildingPosition)){
+    public boolean canBuild(Position buildingPosition) throws NullPointerException {
+        if(super.canBuild(buildingPosition)){
             //notify();
             return true;
         }
