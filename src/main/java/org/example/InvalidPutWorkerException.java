eea43090bb3436891324296e5d5af0d4484a3f8c
daxus4 <davide.raffaellii@gmail.com>
2020-04-08 18:36:30 +0200

    j

--- src/main/java/org/example/InvalidPutWorkerException.java
+++ src/main/java/org/example/InvalidPutWorkerException.java
@@ -3,6 +3,6 @@ package org.example;
 public class InvalidPutWorkerException extends RuntimeException {
     //Runtime exception which is thrown when someone want to put more that two worker
     public InvalidPutWorkerException(int r, int c, PlayerIndex index) {
-        super(index + "cannot put a worker in : [" + r + "][" + c + "], because he already have 2 worker in your board");
+        super(index + " cannot put a worker in : [" + r + "][" + c + "], because he already have 2 worker in your board");
     }
 }
