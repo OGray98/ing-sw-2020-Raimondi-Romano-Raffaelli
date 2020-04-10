e971850dc12963474dfadb0e7d31a2366162c03a
daxus4 <davide.raffaellii@gmail.com>
2020-04-10 11:42:57 +0200

    BoardChange commented

--- src/main/java/org/example/BoardChange.java
+++ src/main/java/org/example/BoardChange.java
@@ -9,13 +9,19 @@ public class BoardChange {
     private BuildType buildType;
     private boolean cantGoUp;
 
-    BoardChange(boolean canGoUp) {
-        this.cantGoUp = canGoUp;
+    /*
+     * Create a BoardChange with the passed cantGoUp and every other field null
+     */
+    BoardChange(boolean cantGoUp) {
+        this.cantGoUp = cantGoUp;
         this.playerChanges = null;
         this.positionBuild = null;
         this.buildType = BuildType.LEVEL;
     }
 
+    /*
+     * Create a BoardChange with every field null
+     */
     BoardChange() {
         this.cantGoUp = false;
         this.playerChanges = null;
@@ -33,7 +39,9 @@ public class BoardChange {
         return playerChanges;
     }
 
-    public Position getPositionBuild() {
+    public Position getPositionBuild() throws NullPointerException {
+        if (positionBuild == null)
+            throw new NullPointerException("positionBuild");
         return positionBuild;
     }
 
@@ -41,15 +49,29 @@ public class BoardChange {
         return buildType;
     }
 
-    public void addPlayerChanges(Position oldPosition, Position newPosition, PlayerIndex playerIndex) {
+    /*
+     * If playerChange isn't initialized, this method create it. Then add an entry in playerChange with a PositionContainer
+     * which contains oldPosition and newPosition to represent the move and the relative PlayerIndex
+     * It requires a Position which is the oldPosition of movement
+     * It requires a Position which is the newPosition of movement
+     * It requires a PlayerIndex which is the player of movement
+     */
+    public void addPlayerChanges(Position oldPosition, Position newPosition, PlayerIndex playerIndex) throws NullPointerException {
+        if (oldPosition == null)
+            throw new NullPointerException("oldPosition");
+        if (newPosition == null)
+            throw new NullPointerException("newPosition");
+
         PositionContainer posCont = new PositionContainer(oldPosition);
         posCont.put(newPosition);
         if (playerChanges == null)
             playerChanges = new HashMap<>();
         playerChanges.put(posCont, playerIndex);
     }
-
-    public void setBuildPosition(Position buildPosition, BuildType type) {
+    
+    public void setBuildPosition(Position buildPosition, BuildType type) throws NullPointerException {
+        if (positionBuild == null)
+            throw new NullPointerException("positionBuild");
         this.positionBuild = buildPosition;
         this.buildType = type;
     }
