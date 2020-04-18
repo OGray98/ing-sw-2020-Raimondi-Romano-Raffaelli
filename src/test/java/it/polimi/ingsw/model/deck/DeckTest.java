eaebcaf6a3f4917d02ee03cb6380abbe2fd56845
OGray98 <61707509+OGray98@users.noreply.github.com>
2020-04-18 18:04:12 +0200

    Deck test

--- src/test/java/it/polimi/ingsw/model/deck/DeckTest.java
+++ src/test/java/it/polimi/ingsw/model/deck/DeckTest.java
@@ -2,15 +2,14 @@ package it.polimi.ingsw.model.deck;
 
 import it.polimi.ingsw.exception.InvalidNumberCardsChosenException;
 import it.polimi.ingsw.exception.WrongGodNameException;
-import it.polimi.ingsw.model.deck.CardInterface;
-import it.polimi.ingsw.model.deck.Deck;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 
 public class DeckTest {
@@ -24,6 +23,32 @@ public class DeckTest {
         deckTwoPlayers = new Deck(2);
         deckThreePlayers = new Deck(3);
         deckEmpty = new Deck(2);
+
+    }
+
+    @Test
+    public void testIfConstructorIsCorrect(){
+
+        assertEquals("Apollo",deckEmpty.getGodCards().get(0).getGodName());
+        assertEquals("Your Worker may move into an opponent Worker’s space by forcing their Worker to the space yours just vacated.",deckEmpty.getGodCards().get(0).getGodDescription());
+        assertEquals("Artemis",deckEmpty.getGodCards().get(1).getGodName());
+        assertEquals("Your Worker may move one additional time, but not back to its initial space.",deckEmpty.getGodCards().get(1).getGodDescription());
+        assertEquals("Athena",deckEmpty.getGodCards().get(2).getGodName());
+        assertEquals("If one of your Workers moved up on your last turn, opponent Workers cannot move up this turn.",deckEmpty.getGodCards().get(2).getGodDescription());
+        assertEquals("Atlas",deckEmpty.getGodCards().get(3).getGodName());
+        assertEquals("Your Worker may build a dome at any level.",deckEmpty.getGodCards().get(3).getGodDescription());
+        assertEquals("Demeter",deckEmpty.getGodCards().get(4).getGodName());
+        assertEquals("Your Worker may build one additional time, but not on the same space.",deckEmpty.getGodCards().get(4).getGodDescription());
+        assertEquals("Hephaestus",deckEmpty.getGodCards().get(5).getGodName());
+        assertEquals("Your Worker may build one additional block (not dome) on top of your first block.",deckEmpty.getGodCards().get(5).getGodDescription());
+        assertEquals("Minotaur",deckEmpty.getGodCards().get(6).getGodName());
+        assertEquals("our Worker may move into an opponent Worker’s space, if their Worker can be forced one space straight backwards to an unoccupied space at any level.",deckEmpty.getGodCards().get(6).getGodDescription());
+        assertEquals("Pan",deckEmpty.getGodCards().get(7).getGodName());
+        assertEquals("You also win if your Worker moves down two or more levels.",deckEmpty.getGodCards().get(7).getGodDescription());
+        assertEquals("Prometheus",deckEmpty.getGodCards().get(8).getGodName());
+        assertEquals("If your Worker does not move up, it may build both before and after moving.",deckEmpty.getGodCards().get(8).getGodDescription());
+
+
 
     }
 
@@ -44,11 +69,11 @@ public class DeckTest {
 
         for(CardInterface c : deckTwoPlayers.getGodCards()){
             deckTwoPlayers.playerGodLikeChoose(c);
-            assertEquals(true,c.getBoolChosenGod());
+            assertTrue(c.getBoolChosenGod());
         }
         for(CardInterface c : deckThreePlayers.getGodCards()){
             deckThreePlayers.playerGodLikeChoose(c);
-            assertEquals(true,c.getBoolChosenGod());
+            assertTrue(c.getBoolChosenGod());
         }
     }
 
