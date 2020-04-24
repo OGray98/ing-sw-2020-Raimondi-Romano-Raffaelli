0ba7612a5cc3460b568e1645485276153cccb75f
daxus4 <davide.raffaellii@gmail.com>
2020-04-24 18:35:38 +0200

    finish part of GameManager relative to the lobby. Update part of GameManager relative to the god phase.

--- src/main/java/it/polimi/ingsw/controller/GameManager.java
+++ src/main/java/it/polimi/ingsw/controller/GameManager.java
@@ -24,7 +24,7 @@ public class GameManager implements Observer<Message> {
     private final GameLobby lobby;
     private Game gameInstance;
     private GodPhaseManager godPhaseManager;
-    private Map<PlayerIndex, RemoteView> remoteViews;
+    private final Map<PlayerIndex, RemoteView> remoteViews;
 
     public GameManager() {
         remoteViews = new HashMap<>();
@@ -84,28 +84,31 @@ public class GameManager implements Observer<Message> {
         godPhaseManager.playerChooseGod(message.getGodName());
     }
 
+    /**
+     * This method communicates with model and set the cards chosen
+     * by player god like. Respond with an error message if there
+     * are problems
+     *
+     * @param message input message. Must be corrected
+     */
     private void handleGodLikeChoseMessage(GodLikeChoseMessage message) {
 
         PlayerIndex clientIndex = message.getClient();
         List<String> godNames = message.getGodNames();
 
         if (!clientIndex.equals(godPhaseManager.getGodLikePlayerIndex())) {
-            remoteViews.get(clientIndex).putMessage(
-                    new ErrorMessage(
-                            clientIndex,
-                            "You aren't player god like",
-                            TypeMessage.NOT_BE_GOD_LIKE
-                    )
+            respondToRemoteView(
+                    clientIndex,
+                    "You aren't player god like",
+                    TypeMessage.NOT_BE_GOD_LIKE
             );
             return;
         }
         if (message.getGodNames().size() != gameInstance.getPlayers().size()) {
-            remoteViews.get(clientIndex).putMessage(
-                    new ErrorMessage(
-                            clientIndex,
-                            "You must choose " + gameInstance.getPlayers().size() + " cards",
-                            TypeMessage.WRONG_NUMBER_CARDS
-                    )
+            respondToRemoteView(
+                    clientIndex,
+                    "You must choose " + gameInstance.getPlayers().size() + " cards",
+                    TypeMessage.WRONG_NUMBER_CARDS
             );
             return;
         }
@@ -113,18 +116,22 @@ public class GameManager implements Observer<Message> {
         gameInstance.setGodsChosenByGodLike(godPhaseManager.getGodsChosen());
     }
 
+    /**
+     * This method set in lobby the players number of this match. Respond with an error message
+     * if there are problems
+     *
+     * @param message input message. Must be corrected
+     */
     private void handleIsThreePlayersGameMessage(TypeMatchMessage message) {
 
         PlayerIndex clientIndex = message.getClient();
         boolean isThreePlayerGame = message.isThreePlayersMatch();
 
         if (!clientIndex.equals(PlayerIndex.PLAYER0)) {
-            remoteViews.get(clientIndex).putMessage(
-                    new ErrorMessage(
-                            clientIndex,
-                            "You can't choose the number of players",
-                            TypeMessage.CANT_CHOOSE_PLAYERS_NUMBER
-                    )
+            respondToRemoteView(
+                    clientIndex,
+                    "You can't choose the number of players",
+                    TypeMessage.CANT_CHOOSE_PLAYERS_NUMBER
             );
             return;
         }
@@ -132,47 +139,71 @@ public class GameManager implements Observer<Message> {
         lobby.setThreePlayersGame(message.isThreePlayersMatch());
     }
 
+    /**
+     * Add a player in the lobby. Respond with an error message
+     * if there are problems
+     *
+     * @param message input message. Must be corrected
+     */
     private void handleNicknameMessage(NicknameMessage message) {
 
         PlayerIndex clientIndex = message.getClient();
         String name = message.getNickname();
 
         if (lobby.isNameAlreadyTaken(name)) {
-            remoteViews.get(clientIndex).putMessage(
-                    new ErrorMessage(
-                            clientIndex,
-                            "There already is a player named " + name,
-                            TypeMessage.NICKNAME
-                    )
+            respondToRemoteView(
+                    clientIndex,
+                    "There already is a player named " + name,
+                    TypeMessage.NICKNAME
             );
             return;
         }
 
-        /*
-        if(lobby.isAlreadyPresent(clientIndex)){
-            remoteViews.get(clientIndex).putMessage(
-                    new ErrorMessage(
-                            message.getClient(),
-                            "You already set your name" + name,
-                            TypeMessage.ALREADY_SET_NICKNAME
-                    )
+        if (lobby.isPlayerAlreadyInLobby(clientIndex)) {
+            respondToRemoteView(
+                    clientIndex,
+                    "You already set your name" + name,
+                    TypeMessage.ALREADY_SET_NICKNAME
             );
             return;
         }
-        */
 
-        lobby.addPlayer(name);
+        lobby.addPlayer(clientIndex, name);
 
         if (lobby.isFull()) {
             List<PlayerInterface> players = new ArrayList<>(lobby.getLobbyPlayers().size());
-            for (int i = 0; i < lobby.getLobbyPlayers().size(); i++)
-                players.add(new Player(lobby.getLobbyPlayers().get(i), PlayerIndex.values()[i]));
+            lobby.getLobbyPlayers().forEach(
+                    (index, nickname) -> players.add(new Player(nickname, index))
+            );
             gameInstance = Game.getInstance(players);
             godPhaseManager = new GodPhaseManager(gameInstance);
         }
     }
 
+    /**
+     * Check if the message is sent by the current player
+     *
+     * @param msg input message. Must be corrected
+     * @return true iff message is sent by the current player
+     */
     private boolean isMessageSentByCurrentPlayer(Message msg) {
         return msg.getClient().equals(gameInstance.getCurrentPlayerIndex());
+    }
+
+    /**
+     * Send an error message at the RemoteView of clientIndex
+     *
+     * @param clientIndex  receiver
+     * @param text         message text
+     * @param specificType specific type of this error message
+     */
+    private void respondToRemoteView(PlayerIndex clientIndex, String text, TypeMessage specificType) {
+        remoteViews.get(clientIndex).putMessage(
+                new ErrorMessage(
+                        clientIndex,
+                        text,
+                        specificType
+                )
+        );
     }
 }
