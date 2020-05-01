8349008fec95c70e47adece7156296d681cdafe2
OGray98 <61707509+OGray98@users.noreply.github.com>
2020-05-01 14:42:04 +0200

    Server change

--- src/main/java/it/polimi/ingsw/network/Server.java
+++ src/main/java/it/polimi/ingsw/network/Server.java
@@ -17,7 +17,7 @@ public class Server {
     private static final int PORT = 12345;
 
     private ServerSocket serverSocket;
-    private ExecutorService executor = Executors.newFixedThreadPool(129);
+    private ExecutorService executor = Executors.newFixedThreadPool(3);
     private Map<PlayerIndex, ClientConnection> waitingConnection = new HashMap<>();
     private Map<ClientConnection, ClientConnection> playingConnection = new HashMap<>();
     private static int lobbyCount = 0;
@@ -41,36 +41,31 @@ public class Server {
         }
     }
 
-    public synchronized void lobby(ClientConnection c){
-       if(lobbyCount == 0){
-           waitingConnection.put(PlayerIndex.PLAYER0,c);
-           ClientConnection c1 = waitingConnection.get(0);
-           RemoteView player1View = new RemoteView(PlayerIndex.PLAYER0,c1);
-           controller.addRemoteView(PlayerIndex.PLAYER0,player1View);
-           lobbyCount++;
-       }
-       else if(lobbyCount != 0 && controller.getPlayerNum() == 2){
-           waitingConnection.put(PlayerIndex.PLAYER1,c);
-           if(waitingConnection.size() == 2){
-               ClientConnection c2 = waitingConnection.get(1);
-               RemoteView player2View = new RemoteView(PlayerIndex.PLAYER1,c2);
-               controller.addRemoteView(PlayerIndex.PLAYER1,player2View);
-               lobbyCount = 0;
-           }
-       }else if(lobbyCount != 0 && controller.getPlayerNum() == 3){
-           waitingConnection.put(PlayerIndex.PLAYER1,c);
-           waitingConnection.put(PlayerIndex.PLAYER2,c);
-           if(waitingConnection.size() == 3){
-               ClientConnection c2 = waitingConnection.get(1);
-               ClientConnection c3 = waitingConnection.get(2);
-               RemoteView player2View = new RemoteView(PlayerIndex.PLAYER1,c2);
-               RemoteView player3View = new RemoteView(PlayerIndex.PLAYER2,c3);
-               controller.addRemoteView(PlayerIndex.PLAYER1,player2View);
-               controller.addRemoteView(PlayerIndex.PLAYER2,player3View);
-               lobbyCount = 0;
-           }
-
-       }
+    public synchronized void lobby(ClientConnection c) {
+        if (lobbyCount == 0) {
+            waitingConnection.put(PlayerIndex.PLAYER0, c);
+            ClientConnection c1 = waitingConnection.get(0);
+            RemoteView player1View = new RemoteView(PlayerIndex.PLAYER0, c1);
+            controller.addRemoteView(PlayerIndex.PLAYER0, player1View);
+            lobbyCount++;
+        } else {
+            waitingConnection.put(PlayerIndex.PLAYER1, c);
+            waitingConnection.put(PlayerIndex.PLAYER2, c);
+            if (controller.getPlayerNum() == 2 && waitingConnection.size() == 2) {
+                ClientConnection c2 = waitingConnection.get(1);
+                RemoteView player2View = new RemoteView(PlayerIndex.PLAYER1, c2);
+                controller.addRemoteView(PlayerIndex.PLAYER1, player2View);
+                lobbyCount = 0;
+            } else if (controller.getPlayerNum() == 3 && waitingConnection.size() == 3) {
+                ClientConnection c2 = waitingConnection.get(1);
+                ClientConnection c3 = waitingConnection.get(2);
+                RemoteView player2View = new RemoteView(PlayerIndex.PLAYER1, c2);
+                RemoteView player3View = new RemoteView(PlayerIndex.PLAYER2, c3);
+                controller.addRemoteView(PlayerIndex.PLAYER1, player2View);
+                controller.addRemoteView(PlayerIndex.PLAYER2, player3View);
+                lobbyCount = 0;
+            }
+        }
     }
 
     public Server() throws IOException{
