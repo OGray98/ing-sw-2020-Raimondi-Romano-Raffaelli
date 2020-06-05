f972f1e2898ac74f8b3cc0922f363720e9bfb173
OGray98 <61707509+OGray98@users.noreply.github.com>
2020-06-05 11:40:17 +0200

    resolved hera icon god and other

--- src/main/java/it/polimi/ingsw/view/GodIconDialog.java
+++ src/main/java/it/polimi/ingsw/view/GodIconDialog.java
@@ -66,8 +66,8 @@ public class GodIconDialog extends GameDialog implements ActionListener {
         godProfile.put("Zeus","Your worker may, build a block under itself.");
         godProfile.put("Hestia","Your worker may build one additional time, but this cannot be on a perimeter space.");
         godProfile.put("Triton","Each time your worker moves into a, perimeter space it may immediately move again.");
-        godProfile.put("Charon","Before your worker moves, you may force a neighboring opponent worker to the space directly on the other side of your worker, if that space is unoccupied.");
-        godProfile.put("Hera","An opponent cannot win by moving into a perimeter space.");
+        godProfile.put("Charon","Before your worker moves you may force a neighboring opponent worker, to the space directly on the other side of your worker if that space is unoccupied.");
+        godProfile.put("Hera","An opponent cannot win, by moving into a perimeter space.");
     }
 
     private String getPower(String nameGod){
