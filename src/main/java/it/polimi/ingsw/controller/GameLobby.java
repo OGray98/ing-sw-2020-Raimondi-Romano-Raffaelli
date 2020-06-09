d5bd754880466e71fb447e605e0b939505dec6c6
romano-francesco <francesco6.romano@mail.polimi.it>
2020-06-09 15:06:28 +0200

    Fix in handling client connections

--- src/main/java/it/polimi/ingsw/controller/GameLobby.java
+++ src/main/java/it/polimi/ingsw/controller/GameLobby.java
@@ -10,6 +10,7 @@ public class GameLobby {
 
     private final HashMap<PlayerIndex, String> lobbyPlayers;
     private boolean threePlayersGame;
+    private boolean threePlayersGameDecided = false;
 
     public int getNumberOfPlayerInLobby(){
        if(!threePlayersGame){
@@ -40,10 +41,11 @@ public class GameLobby {
      * @throws IllegalArgumentException  if the player is already in the lobby
      */
     public void addPlayer(PlayerIndex playerIndex, String playerNick) {
-        if(isFull()) throw new MaxPlayersException();
+        if(isFull() && threePlayersGameDecided) throw new MaxPlayersException();
         if(isNameAlreadyTaken(playerNick)) throw new NameAlreadyTakenException();
         if(isPlayerAlreadyInLobby(playerIndex)) throw new IllegalArgumentException("Player " + playerIndex + " is already in the lobby");
 
+        if(lobbyPlayers.size() == 3) return;
         this.lobbyPlayers.put(playerIndex, playerNick);
     }
 
@@ -72,6 +74,10 @@ public class GameLobby {
             return this.lobbyPlayers.size() == 3;
         }
         return this.lobbyPlayers.size() == 2;
+    }
+
+    public void setThreePlayersGameDecided(boolean threePlayersGameDecided) {
+        this.threePlayersGameDecided = threePlayersGameDecided;
     }
 
     public void removeFromLobby(PlayerIndex playerToRemove){
