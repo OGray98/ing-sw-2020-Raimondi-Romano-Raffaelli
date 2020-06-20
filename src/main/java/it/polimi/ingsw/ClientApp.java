6c5a1e7ea152180c0bea5027219d41988de235f2
daxus4 <davide.raffaellii@gmail.com>
2020-06-20 16:44:47 +0200

    set args in clientmain

--- src/main/java/it/polimi/ingsw/ClientApp.java
+++ src/main/java/it/polimi/ingsw/ClientApp.java
@@ -3,7 +3,6 @@ package it.polimi.ingsw;
 import it.polimi.ingsw.network.Client;
 
 import java.io.IOException;
-import java.util.Scanner;
 
 public class ClientApp {
 
@@ -13,18 +12,22 @@ public class ClientApp {
     public static void main(String[] args) {
         try {
 
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Do you want to use CLI or GUI?");
-            System.out.println("Type \"c\" for CLI, \"g\" for GUI");
+            String type = "";
 
-            String typed = scanner.next();
-            while (!typed.equals(GUI) && !typed.equals(CLI)) {
-                System.out.println("Type \"c\" for CLI, \"g\" for GUI");
-                typed = scanner.next();
+            if (args.length == 0)
+                type = GUI;
+            else if (args.length == 1) {
+                if (args[0].equals("-c"))
+                    type = CLI;
+                else
+                    System.out.println("Argument acceptable : -c");
+            } else
+                System.out.println("Argument acceptable : -c");
+
+            if (type.equals(GUI) || type.equals(CLI)) {
+                Client client = new Client(type);
+                client.run();
             }
-
-            Client client = new Client(typed);
-            client.run();
         } catch (IOException e) {
             System.err.println("Impossible to initialize the server: " + e.getMessage() + "!");
         }
