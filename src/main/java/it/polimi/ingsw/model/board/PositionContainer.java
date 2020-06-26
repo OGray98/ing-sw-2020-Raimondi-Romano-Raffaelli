fc32b398545e7d6df26dd5ed5f3e4218e3441fcd
romano-francesco <francesco6.romano@mail.polimi.it>
2020-06-26 19:14:05 +0200

    added javadoc

--- src/main/java/it/polimi/ingsw/model/board/PositionContainer.java
+++ src/main/java/it/polimi/ingsw/model/board/PositionContainer.java
@@ -1,5 +1,8 @@
 package it.polimi.ingsw.model.board;
 
+/**
+ * Class used to create a set of two positions useful in some cases
+ * */
 public class PositionContainer {
     private Position[] pos = new Position[2];
 
