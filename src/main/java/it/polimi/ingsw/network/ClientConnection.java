1f1bd15e3c4170a27a143e4919205506ebcc5a52
OGray98 <61707509+OGray98@users.noreply.github.com>
2020-06-28 18:42:11 +0200

    NetWork javadoc

--- src/main/java/it/polimi/ingsw/network/ClientConnection.java
+++ src/main/java/it/polimi/ingsw/network/ClientConnection.java
@@ -13,17 +13,43 @@ import java.io.IOException;
  */
 public interface ClientConnection {
 
+    /**
+     * Close the socket, input-stream and output-stream of connection
+     */
     void closeConnection();
 
+    /**
+     * @param observer
+     * add observer
+     */
     void addObserver(Observer<MessageToServer> observer);
 
+    /**
+     * @param message
+     * send a response message to client from server
+     */
     void asyncSend(MessageToClient message);
 
+    /**
+     * @param clientIndex
+     * set client index
+     */
     void setClientIndex(PlayerIndex clientIndex);
 
+    /**
+     * @return boolean true if server is connected to client
+     */
     boolean isConnected();
 
+    /**
+     * @param player of relative client
+     * @throws IOException error on input or output stream
+     *thread that send a ping message to client to verify if he is alive
+     */
     void ping(PlayerIndex player) throws IOException;
 
+    /**
+     * set the connection to close
+     */
     void forceDisconnection();
 }
--- src/main/java/it/polimi/ingsw/network/ServerConnection.java
+++ src/main/java/it/polimi/ingsw/network/ServerConnection.java
@@ -2,6 +2,9 @@ package it.polimi.ingsw.network;
 
 import it.polimi.ingsw.message.MessageToServer;
 
+/**
+ * This interface is used by client to send message to server
+ */
 public interface ServerConnection {
     void sendToServer(MessageToServer message);
 }
